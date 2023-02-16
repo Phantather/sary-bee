2dfcc763446e6101ff59aad00b1cc7792ddc3a42
Phantather <akdil.hstech@gmail.com>
2023-02-16 13:30:12 +0600

    add all filses for first psge

--- src/js/main.js
+++ src/js/main.js
@@ -43,39 +43,39 @@ $('ul.acc-list').on('click', 'li:not(.active)', function() {
         .closest('div.tabs').find('div.acc-tow').removeClass('active').eq($(this).index()).addClass('active');
 });
 
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 3,
-//     spaceBetween: 30,
-//     loop: true,
-//     autoplay:{
-//         delay: 2000
-//     },
-//     speed: 1500,
-//     breakpoints: {
-//         1200: {
-//             slidesPerView: 3
-//         },
-//         768: {
-//             slidesPerView: 2
-//         },
-//         676: {
-//           slidesPerView: 2
-//         },
-//         641: {
-//             slidesPerView: 1
-//         },
-//         576: {
-//             slidesPerView: 1
-//         },
-//         425: {
-//             slidesPerView: 1
-//         },
-//         375: {
-//             slidesPerView: 1
-//         },
-//         320: {
-//             slidesPerView: 1
-//         }
-//     }
-// });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    autoplay:{
+        delay: 2000
+    },
+    speed: 1500,
+    breakpoints: {
+        1200: {
+            slidesPerView: 3
+        },
+        768: {
+            slidesPerView: 2
+        },
+        676: {
+          slidesPerView: 2
+        },
+        641: {
+            slidesPerView: 1
+        },
+        576: {
+            slidesPerView: 1
+        },
+        425: {
+            slidesPerView: 1
+        },
+        375: {
+            slidesPerView: 1
+        },
+        320: {
+            slidesPerView: 1
+        }
+    }
+});
 
